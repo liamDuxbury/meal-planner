10a667804afb12f18ced20edcab3faceaabfc4e7
Liam Duxbury <liam.duxbury@envolve.com>
2026-09-05 21:17:07 +0100

    Add recipes endpoint and route

--- meal_planner/routes.py
+++ meal_planner/routes.py
@@ -2,8 +2,17 @@
 from flask import render_template
 
 from meal_planner.app import app
+from meal_planner.models.meal_planning import Recipe
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/recipes', methods=['GET'])
+def recipes():
+    recipes = Recipe.query.all()
+    if not recipes:
+        recipes = []
+    
+    return render_template('recipes.html', recipes=recipes)

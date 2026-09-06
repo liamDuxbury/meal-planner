c5928576565bdcdc377c77e809a15b98f38542c4
Liam Duxbury <liam.duxbury@envolve.com>
2026-09-06 22:40:53 +0100

    Improve cuisine selection UX UI

--- static/js/meal_plan_controller.js
+++ static/js/meal_plan_controller.js
@@ -1,22 +1,35 @@
 const MealPlanController = {
-    init(generateUrl, containerId, buttonId, overrideExistingCheckboxId, cuisineFilterId) {
+    init({ generateUrl, containerId, buttonId, overrideCheckboxId, cuisineToggleId, cuisineOptionsId }) {
         this.generateUrl = generateUrl;
         this.container = document.getElementById(containerId);
-        this.overrideExistingCheckbox = document.getElementById(overrideExistingCheckboxId);
-        this.cuisineFilter = document.getElementById(cuisineFilterId);
+        this.overrideCheckbox = document.getElementById(overrideCheckboxId);
+        this.cuisineToggle = document.getElementById(cuisineToggleId);
+        this.cuisineOptions = document.getElementById(cuisineOptionsId);
+
+        this.cuisineToggle.addEventListener('change', () => {
+            this.cuisineOptions.disabled = !this.cuisineToggle.checked;
+        });
+
         document.getElementById(buttonId).addEventListener('click', () => this.generate());
     },
 
-    generate() {
-        const overrideExisting = this.overrideExistingCheckbox.checked;
-        const cuisines = Array.from(this.cuisineFilter.selectedOptions).map(option => option.value);
+    selectedCuisines() {
+        if (!this.cuisineToggle.checked) {
+            return [];
+        }
+        return Array.from(this.cuisineOptions.querySelectorAll('input:checked')).map(input => input.value);
+    },
 
+    generate() {
         fetch(this.generateUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ overrideExisting, cuisines }),
+            body: JSON.stringify({
+                overrideExisting: this.overrideCheckbox.checked,
+                cuisines: this.selectedCuisines(),
+            }),
         })
             .then(response => response.text())
             .then(html => this.render(html))

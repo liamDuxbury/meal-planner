4af1f5b0fd152a0d4367055353697bdeca9076f4
Liam Duxbury <liam.duxbury@envolve.com>
2026-09-06 21:46:52 +0100

    Ensure meal plan starts for next monday

--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
 """Module for defining routes in the meal planner application."""
 import random
-from datetime import date, timedelta
+from datetime import datetime, timedelta
 
 from flask import jsonify, render_template, request
 
 from app import app, db
 from models.meal_planning import MealPlan, MealPlanRecipe, Recipe
+
 
 @app.route('/')
 def index():
@@ -34,27 +35,34 @@ def meal_plan():
 
 @app.route('/meal-plan/generate', methods=['POST'])
 def generate_meal_plan():
-    # Get recipes
     data = request.get_json()
     override_existing = data.get('overrideExisting', False) if data else False
-    print(f"Override existing meal plan: {override_existing}")
-    print(f"Request data: {data}")
-    recipes = Recipe.query.all()
-    if len(recipes) < 7:
-        return jsonify(error="Not enough recipes to generate a meal plan, run seed_recipes.sh"), 400
-    # Select 7 random recipes
-    selected = random.sample(recipes, 7)
-    # Create a new meal plan with the selected recipes
-    today = date.today()
-    existing_meal_plan = MealPlan.query.filter(MealPlan.name == f"Meal plan {today.isoformat()}").first()
+
+    today = datetime.today().date()
+    days_until_next_monday = 7 - today.weekday()
+    week_commencing_date = today + timedelta(days=days_until_next_monday)
+
+    existing_meal_plan = MealPlan.query.filter(MealPlan.name == f"Meal plan {week_commencing_date.isoformat()}").first()
     if existing_meal_plan:
         if not override_existing:
             return jsonify(error="Meal plan for today already exists."), 409
         db.session.delete(existing_meal_plan)
-    meal_plan = MealPlan(name=f"Meal plan {today.isoformat()}")
-    meal_plan.recipes = [MealPlanRecipe(recipe_id=recipe.id, date=today + timedelta(days=i)) for i, recipe in enumerate(selected)]
-    # Save the meal plan to the database
+
+    meal_plan = generate_meal_plan_for_date(week_commencing_date)
+    return render_template('_meal_plan.html', meal_plan=meal_plan)
+
+
+def generate_meal_plan_for_date(target_date):
+    recipes = Recipe.query.all()
+
+    if len(recipes) < 7:
+        raise ValueError("Not enough recipes to generate a meal plan.")
+
+    selected = random.sample(recipes, 7)
+    meal_plan = MealPlan(name=f"Meal plan {target_date.isoformat()}")
+    meal_plan.recipes = [MealPlanRecipe(recipe_id=recipe.id, date=target_date + timedelta(days=i)) for i, recipe in enumerate(selected)]
+
     db.session.add(meal_plan)
     db.session.commit()
-    # Return the generated meal plan
-    return render_template('_meal_plan.html', meal_plan=meal_plan)
+
+    return meal_plan

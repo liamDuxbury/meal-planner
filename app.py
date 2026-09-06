80626145b39c7847e6227d7e984b189081cb8bd1
Liam Duxbury <liam.duxbury@envolve.com>
2026-09-06 20:00:45 +0100

    Re-add missing alembic migration setup in app

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-
-# Avoid using relative paths, causes issues when running migrations
 basedir = os.path.abspath(os.path.dirname(__file__))
-project_root = os.path.dirname(basedir)
-DATABASE_URI = f"sqlite:///{os.path.join(project_root, 'instance', 'meal_planner.db')}"
+DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance', 'meal_planner.db')}"
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-import routes  # noqa: F401, E402
+import routes  # noqa: E402, F401
